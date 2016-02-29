7da74cd8a00047e9a730286a5803650ed37ce0c8
Pavel <pavelnerd@gmail.com>
2016-02-29 20:42:03 +0300

    add fullwidth narrow template

--- kmk-project/kmk/settings.py
+++ kmk-project/kmk/settings.py
@@ -191,6 +191,7 @@ CMS_LANGUAGES = {
 CMS_TEMPLATES = (
     ## Customize this
     ('fullwidth.html', 'Fullwidth'),
+    ('fullwidth_narrow.html', 'Fullwidth Narrow'),
     ('sidebar_left.html', 'Sidebar Left'),
     # ('sidebar_left_submenu.html', 'Sidebar Left (Submenu)'),
     # ('sidebar_right.html', 'Sidebar Right'),
